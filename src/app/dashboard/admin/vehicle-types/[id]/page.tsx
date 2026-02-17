dfc9b4893e56f167edb2105a2d656c44a1fdc901
PIETRO CIRELLA <cirella.pietro@gmail.com>
2026-02-17 07:34:20 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/dashboard/admin/vehicle-types/[id]/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is intentionally left non-functional to resolve a Next.js routing conflict.
-// By not exporting a component, we signal to Next.js that this should not be treated as a page.
