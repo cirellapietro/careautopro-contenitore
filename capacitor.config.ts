b399caf29336c4f877dc8f6d69627a0211e089c4
PIETRO CIRELLA <cirella.pietro@gmail.com>
2026-02-17 22:29:02 +0000

    Add: configurazione Capacitor per APK

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.careautopro.app',
+  appName: 'CareAutoPro',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
