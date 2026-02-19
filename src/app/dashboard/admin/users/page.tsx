aefcc5a5d36c046e02257e42b35f322c133a36ea
cirellapietro <133475613+cirellapietro@users.noreply.github.com>
2026-02-19 01:58:37 +0100

    Remove duplicate 'use client' declaration

--- src/app/dashboard/admin/users/page.tsx
+++ src/app/dashboard/admin/users/page.tsx
@@ -1,7 +1,7 @@
+'use client';
 export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 
-'use client';
 import { useUser } from "@/firebase/auth/use-user";
 import { useFirebase, useCollection, errorEmitter, FirestorePermissionError } from "@/firebase";
 import { collection, doc, updateDoc, query, where } from 'firebase/firestore';
